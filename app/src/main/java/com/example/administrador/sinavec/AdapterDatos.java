68f535655138245b062d579fce9ad3c02d31f604
jesus <javentura@salud.gob.sv>
2018-07-26 09:09:20 -0600

    Se inicializa la estructura del proyecto Android Malaria

---
+++ app/src/main/java/com/example/administrador/sinavec/AdapterDatos.java
@@ -0,0 +1,92 @@
+package com.example.administrador.sinavec;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.administrador.sinavec.clases.Utilidades;
+
+import java.util.ArrayList;
+
+/**
+ * Created by administrador on 07-23-18.
+ */
+
+public class AdapterDatos
+        extends RecyclerView.Adapter<AdapterDatos.ViewHolderDatos>
+        implements View.OnClickListener{
+
+    ArrayList<PersonajeVo> listDatos;
+    private View.OnClickListener listener;
+
+    public AdapterDatos(ArrayList<PersonajeVo> listDatos) {
+        this.listDatos = listDatos;
+    }
+
+        //Enlazara el presente adaptador con el xml item_list
+    @Override
+    public ViewHolderDatos onCreateViewHolder(ViewGroup parent, int viewType) {
+        int layout=0;
+        if(Utilidades.visualizacion==Utilidades.LIST){
+            layout= R.layout.item_list;
+
+        }else{
+            layout=R.layout.item_grid_list;
+        }
+
+        View view= LayoutInflater.from(parent.getContext())
+                .inflate(layout,null,false);
+        //permite que se escuche el evento de seleccion
+        view.setOnClickListener(this);
+
+        return new ViewHolderDatos(view);
+    }
+
+        //comunicara el adaptador en tre la clase ViewHolderDatos
+    @Override
+    public void onBindViewHolder(ViewHolderDatos holder, int position) {
+        holder.idNombre.setText(listDatos.get(position).getNombre());
+        if(Utilidades.visualizacion==Utilidades.LIST){
+            holder.idInfo.setText(listDatos.get(position).getInfo());
+        }
+        holder.foto.setImageResource(listDatos.get(position).getFoto());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listDatos.size();
+    }
+
+
+    public void setOnClickListener(View.OnClickListener listener){
+        this.listener=listener;
+    }
+    @Override
+    public void onClick(View view) {
+        if (listener!=null){
+            listener.onClick(view);
+        }
+    }
+
+    public class ViewHolderDatos extends RecyclerView.ViewHolder {
+
+        TextView idNombre,idInfo;
+        ImageView foto;
+        public ViewHolderDatos(View itemView) {
+            super(itemView);
+            //hace la referencia al contenido que se cargara
+            idNombre=(TextView) itemView.findViewById(R.id.idNombre);
+            if(Utilidades.visualizacion==Utilidades.LIST){
+                idInfo=(TextView)itemView.findViewById(R.id.idInfo);
+            }
+            foto=(ImageView) itemView.findViewById(R.id.foto);
+        }
+
+        /*public void asignarDatos(String datos) {
+            dato.setText(datos);
+        }*/
+    }
+}
---
+++ app/src/main/java/com/example/administrador/sinavec/fragment/ColvolFragment.java
@@ -0,0 +1,18 @@
+package com.example.administrador.sinavec.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.administrador.sinavec.R;
+
+public class ColvolFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_colvol, container, false);
+    }
+}
