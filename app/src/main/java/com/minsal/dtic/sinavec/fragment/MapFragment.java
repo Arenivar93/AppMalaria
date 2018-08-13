7677f13972e874764d13d00ae3cb21e48d541352
jesus <javentura@salud.gob.sv>
2018-08-13 13:23:38 -0600

    merge

--- app/src/main/java/com/minsal/dtic/sinavec/fragment/MapFragment.java
+++ app/src/main/java/com/minsal/dtic/sinavec/fragment/MapFragment.java
@@ -121,14 +121,14 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, View.On
             showInfoAlert();
         } else {
             if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
+                return;
             }
             Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if(location==null){
+            if (location == null) {
                 location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
             }
-            currentLocation=location;
-            if(currentLocation != null){
+            currentLocation = location;
+            if (currentLocation != null) {
                 createOrUpdateMarkerByLocation(location);
                 zoomToLocation(location);
             }
@@ -136,11 +136,12 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, View.On
         }
     }
 
-    private void createOrUpdateMarkerByLocation(Location location){
-        if (marker == null){
-            marker=gmap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(),location.getLongitude())).draggable(true));
-        }else {
-            marker.setPosition(new LatLng(location.getLatitude(),location.getLongitude()));
+    private void createOrUpdateMarkerByLocation(Location location) {
+        if (marker == null) {
+            marker = gmap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).draggable(true));
+        } else {
+            marker.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
+            zoomToLocation(location);
         }
     }
 
@@ -166,9 +167,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, View.On
 
     }
 
-    public void zoomLocation(Location location){
-        cameraZoom=new CameraPosition.Builder()
-                .target(new LatLng(location.getLatitude(),location.getLongitude()))
+    public void zoomLocation(Location location) {
+        cameraZoom = new CameraPosition.Builder()
+                .target(new LatLng(location.getLatitude(), location.getLongitude()))
                 .zoom(15)
                 .bearing(0)
                 .tilt(30)
@@ -177,36 +178,13 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, View.On
 
     }
 
-//Codigo para obtener permisos para geolocalizacion
-    @SuppressLint("MissingPermission")
+    //Codigo para obtener permisos para geolocalizacion
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
         gmap = googleMap;
-        locationManager=(LocationManager)getContext().getSystemService(Context.LOCATION_SERVICE);
-
-
         setUpMap();
-
-        gmap.setMyLocationEnabled(false);
-        //gmap.getUiSettings().setMyLocationButtonEnabled(false);
-        /*Desabilita el boton que se crea en la
-        interfaz*/
-        /*Boton que se ejecuta al presionar el boton arriba cancelado*/
-        /*gmap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
-            @Override
-            public boolean onMyLocationButtonClick() {
-                Toast.makeText(getContext(),"Mi posicion en mapa",Toast.LENGTH_LONG).show();
-                return false;
-            }
-        });*/
-
-
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, this);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
-
-
-
+        Toast.makeText(getContext(),"Map ready",Toast.LENGTH_LONG).show();
     }
 
     private void zoomToLocation(Location location){
@@ -251,8 +229,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, View.On
                 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             enableMyLocation();
         }else {
-            // permission denied, boo! Disable the
-            // functionality that depends on this permission.
+            Toast.makeText(getContext(),"Permiso denegado!!!",Toast.LENGTH_LONG).show();
         }
     }
     private void enableMyLocation() {
@@ -266,125 +243,10 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, View.On
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+        locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
         gmap.setMyLocationEnabled(true);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, this);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
+        Toast.makeText(getContext(),"Finaliza carga de metodos",Toast.LENGTH_LONG).show();
     }
 }
-
-
-/*
-public class MapFragment extends Fragment implements OnMapReadyCallback,View.OnClickListener{
-
-    private View viewrootView;
-    private MapView mapView;
-    private GoogleMap gmap;
-
-    private FloatingActionButton fab;
-
-    public MapFragment() {
-
-    }
-
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        viewrootView=inflater.inflate(R.layout.fragment_map, container, false);
-        fab=(FloatingActionButton) viewrootView.findViewById(R.id.fab);
-
-        fab.setOnClickListener(this);
-        return viewrootView;
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        mapView=viewrootView.findViewById(R.id.mapFragment);
-
-        if (mapView != null){
-            mapView.onCreate(null);
-            mapView.onResume();
-            mapView.getMapAsync(this);
-        }
-
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        //this.verificarGPS();
-
-    }
-
-    public void verificarGPS(){
-        try {
-            int gpsSignal= Settings.Secure.getInt(getActivity().getContentResolver(),Settings.Secure.LOCATION_MODE);
-            if (gpsSignal==0){
-                //El gps no esta activado
-                //Toast.makeText(getContext(),"Por favor active el GPS",Toast.LENGTH_LONG).show();
-                showInfoAlert();
-            }
-
-        } catch (Settings.SettingNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void showInfoAlert(){
-        new AlertDialog.Builder(getContext())
-                .setTitle("Gps Signal")
-                .setMessage("Desea activar el gps?")
-                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Intent intent=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                        startActivity(intent);
-                    }
-                })
-                .setNegativeButton("No",null)
-                .show();
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-
-        gmap=googleMap;
-        LatLng place=new LatLng(13.71466708231014, -89.20727916061878);
-        gmap.addMarker(new MarkerOptions().position(place).title("Casa ues").draggable(true));
-        gmap.moveCamera(CameraUpdateFactory.newLatLngZoom(place,15));
-        gmap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-
-        gmap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
-            @Override
-            public void onMarkerDragStart(Marker marker) {
-                marker.hideInfoWindow();
-            }
-
-            @Override
-            public void onMarkerDrag(Marker marker) {
-
-            }
-
-            @Override
-            public void onMarkerDragEnd(Marker marker) {
-                Toast.makeText(getContext(),"Drag Click on: \n"+
-                        "Lat:" + marker.getPosition().latitude + "\n" +
-                        "Lon:" + marker.getPosition().longitude, Toast.LENGTH_SHORT).show();
-
-                marker.showInfoWindow();
-            }
-        });
-
-    }
-
-
-    @Override
-    public void onClick(View view) {
-        this.verificarGPS();
-    }
-}*/
-
-
-
-
