8cf36d8c8fb64e9d289e2e896a9bd244c9a2353b
jesus <javentura@salud.gob.sv>
2018-08-16 11:38:49 -0600

    Se agrego la clase EntityGenerator la cual se encargara de generar las entidades y sus daos respectivos por medio de greenDAO

--- greenDaoGenerator/src/main/java/com/minsal/dtic/greendaogenerator/EntityGenerator.java
+++ greenDaoGenerator/src/main/java/com/minsal/dtic/greendaogenerator/EntityGenerator.java
@@ -1,4 +1,37 @@
 package com.minsal.dtic.greendaogenerator;
 
+import org.greenrobot.greendao.generator.DaoGenerator;
+import org.greenrobot.greendao.generator.Entity;
+import org.greenrobot.greendao.generator.Schema;
+
 public class EntityGenerator {
+    public static void main(String[] args) {
+        Schema schema = new Schema(1, "com.minsal.dtic.sinavec.EntityDAO"); // Your app package name and the (.db) is the folder where the DAO files will be generated into.
+        schema.enableKeepSectionsByDefault();
+
+        addTables(schema);
+
+        try {
+            new DaoGenerator().generateAll(schema,"./app/src/main/java");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addTables(final Schema schema) {
+        addUserEntities(schema);
+    }
+
+    // This is use to describe the colums of your table
+    private static Entity addUserEntities(final Schema schema) {
+        Entity user = schema.addEntity("User");
+        user.addIdProperty().primaryKey().autoincrement();
+        user.addIntProperty("user_id").notNull();
+        user.addStringProperty("last_name");
+        user.addStringProperty("first_name");
+        user.addStringProperty("email");
+        return user;
+    }
+
 }
+
