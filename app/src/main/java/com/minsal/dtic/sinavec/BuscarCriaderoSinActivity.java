24278843261fd8773b642986924133b0a0170c9d
jesus <javentura@salud.gob.sv>
2018-08-22 15:40:58 -0600

    Se agrego a una clase el uso de los spinner de municipio,canton y caseio, para que se puedan reutilizar

--- app/src/main/java/com/minsal/dtic/sinavec/BuscarCriaderoSinActivity.java
+++ app/src/main/java/com/minsal/dtic/sinavec/BuscarCriaderoSinActivity.java
@@ -1,13 +1,23 @@
 package com.minsal.dtic.sinavec;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.internal.Objects;
@@ -17,7 +27,10 @@ import com.minsal.dtic.sinavec.EntityDAO.CtlCaserio;
 import com.minsal.dtic.sinavec.EntityDAO.CtlCaserioDao;
 import com.minsal.dtic.sinavec.EntityDAO.CtlMunicipio;
 import com.minsal.dtic.sinavec.EntityDAO.CtlMunicipioDao;
+import com.minsal.dtic.sinavec.EntityDAO.CtlPlCriaderoDao;
 import com.minsal.dtic.sinavec.EntityDAO.DaoSession;
+import com.minsal.dtic.sinavec.EntityDAO.PlColvolDao;
+import com.minsal.dtic.sinavec.utilidades.Utilidades;
 
 import org.greenrobot.greendao.Property;
 
@@ -28,19 +41,26 @@ import HelperDB.DbHelpers;
 
 public class BuscarCriaderoSinActivity extends AppCompatActivity {
     Spinner spMunicipio, spCanton,spCaserio;
+    ImageView buscar;
+    TableLayout tablaCriaderos;
     private SQLiteDatabase conDb;
     private DbHelpers helper;
     DaoSession daoSession;
     CtlMunicipioDao daoMunicipio;
     CtlCantonDao daoCanton;
     CtlCaserioDao daoCaserio;
+    CtlPlCriaderoDao ctlPlCriaderoDao;
 
     ArrayList<String> listaMunicipio=new ArrayList<String>();
-    List<CtlMunicipio> municipios=new ArrayList<CtlMunicipio>();
+    List<CtlMunicipio> municipios;
     ArrayList<String> listaCanton=new ArrayList<String>();
-    List<CtlCanton> cantones=new ArrayList<CtlCanton>();
-    ArrayList<String> listaCaserios;
-    List<CtlCaserio> caserios=new ArrayList<CtlCaserio>();
+    List<CtlCanton> cantones;
+    ArrayList<String> listaCaserios=new ArrayList<String>();
+    List<CtlCaserio> caserios;
+    ArrayAdapter<String> adapter;
+    ArrayAdapter<String> adapter2;
+    ArrayAdapter<String> adapter3;
+    Utilidades utilidades;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,76 +68,169 @@ public class BuscarCriaderoSinActivity extends AppCompatActivity {
         spMunicipio = (Spinner)findViewById(R.id.spMun);
         spCanton = (Spinner)findViewById(R.id.spCan);
         spCaserio = (Spinner)findViewById(R.id.spCas);
-
-        listaMunicipio.add("Seleccione");
+        buscar= (ImageView)findViewById(R.id.buscarCriadero);
+        tablaCriaderos = (TableLayout)findViewById(R.id.tableCriadero);
         listaCanton.add("Seleccione");
+        listaCaserios.add("Seleccione");
 
-        //Cargo session de la bd
         daoSession=((MyMalaria)getApplicationContext()).getDaoSession();
+        utilidades=new Utilidades(daoSession);
 
-        loadSpinerMun();
+        municipios=utilidades.loadspinnerMunicipio(3);
+        listaMunicipio=utilidades.obtenerListaMunicipio(municipios);
 
-        ArrayAdapter<CharSequence> adapter=new ArrayAdapter
+        //loadSpinerMun();
+
+        adapter=new ArrayAdapter
                 (this,android.R.layout.simple_list_item_1,listaMunicipio);
+        adapter.notifyDataSetChanged();
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spMunicipio.setAdapter(adapter);
-
-        final ArrayAdapter<CharSequence> adapter2=new ArrayAdapter
+        adapter2=new ArrayAdapter
                 (this,android.R.layout.simple_list_item_1,listaCanton);
+        adapter2.notifyDataSetChanged();
         spCanton.setAdapter(adapter2);
+        adapter3=new ArrayAdapter
+                (this,android.R.layout.simple_list_item_1,listaCaserios);
+        spCaserio.setAdapter(adapter3);
+        adapter3.notifyDataSetChanged();
 
+
+        encabezadoTabla(getApplicationContext());
+        buscarCriaderos(getApplicationContext());
 
         spMunicipio.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if(position!=0){
-                    //String nombre=municipios.get(position-1).getNombre();
-                    //Toast.makeText(parent.getContext(),nombre,Toast.LENGTH_LONG).show();
-                    loadSpinerCanton(municipios.get(position-1).getId());
+                    cantones=utilidades.loadSpinerCanton(municipios.get(position-1).getId());
+                    listaCanton.clear();
+                    listaCanton=utilidades.obetenerListaCantones(cantones);
+                    adapter2=new ArrayAdapter
+                            (parent.getContext(),android.R.layout.simple_list_item_1,listaCanton);
+                    spCanton.setAdapter(adapter2);
                     adapter2.notifyDataSetChanged();
-
+                    spCanton.setSelection(0);
+                }else{
+                    listaCanton.clear();
+                    listaCanton.add("Seleccione");
+                    adapter2.notifyDataSetChanged();
+                    spCanton.setSelection(0);
+                    listaCaserios.clear();
+                    listaCaserios.add("Seleccione");
+                    adapter3.notifyDataSetChanged();
+                    spCaserio.setSelection(0);
                 }
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
+        spCanton.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position!=0){
+                    caserios=utilidades.loadSpinerCaserio(cantones.get(position-1).getId());
+                    listaCaserios.clear();
+                    listaCaserios=utilidades.obetenerListaCaserios(caserios);
+                    adapter3=new ArrayAdapter
+                            (parent.getContext(),android.R.layout.simple_list_item_1,listaCaserios);
+                    spCaserio.setAdapter(adapter3);
+                    adapter3.notifyDataSetChanged();
+                    spCaserio.setSelection(0);
+                    Toast.makeText(parent.getContext(),cantones.get(position-1).getNombre()+"",Toast.LENGTH_LONG).show();
+                }else{
+                    listaCaserios.clear();
+                    listaCaserios.add("Seleccione");
+                    adapter3.notifyDataSetChanged();
+                    spCaserio.setSelection(0);
+                }
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
+            }
+        });
+        buscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Buscara Criadero",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
-    private void loadSpinerCanton(Long idCtn) {
-        Toast.makeText(this,idCtn+"",Toast.LENGTH_LONG).show();
-        daoCanton=daoSession.getCtlCantonDao();
-        cantones=new ArrayList<CtlCanton>();
-        cantones=daoCanton.queryBuilder().where(CtlCantonDao.Properties.IdMunicipio.eq(idCtn))
-                .orderAsc(CtlCantonDao.Properties.Nombre).list();
-        obetenerListaCantones();
-    }
+    private void buscarCriaderos(Context context) {
+        Toast.makeText(this,"Llena tabla",Toast.LENGTH_LONG).show();
+        try
+        {
+            //daoSession.getDatabase().beginTransaction();
+            String selectQuery = "SELECT id,nombre FROM ctl_pl_criadero";
+            Cursor cursor = daoSession.getDatabase().rawQuery(selectQuery,null);
+            //Toast.makeText(this,cursor.getCount()+"",Toast.LENGTH_LONG).show();
+            while (cursor.moveToNext()) {
+                // Read columns data
+                final int id= cursor.getInt(0);
+                String nombre= cursor.getString(1);
+                //String municipio= cursor.getString(cursor.getColumnIndex("nombre"));
+               // String canton= cursor.getString(cursor.getColumnIndex("nombre"));
+               // String caserio= cursor.getString(cursor.getColumnIndex("nombre"));
 
-    private void obetenerListaCantones() {
-        listaCanton=new ArrayList<String>();
-        Toast.makeText(this,"Llena lista Cantones",Toast.LENGTH_LONG).show();
-
-        for (int i=0;i<cantones.size();i++){
-            listaCanton.add(cantones.get(i).getNombre());
+                // dara rows
+                TableRow row = new TableRow(context);
+                row.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,
+                        TableLayout.LayoutParams.WRAP_CONTENT));
+                String[] colText={nombre,nombre,nombre,nombre};
+                for(String text:colText) {
+                    TextView tv = new TextView(context);
+                    tv.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                            TableRow.LayoutParams.WRAP_CONTENT));
+                    tv.setTextSize(16);
+                    tv.setPadding(85, 5, 85, 5);
+                    tv.setText(text);
+                    tv.setTextColor(Color.BLACK);
+                    row.addView(tv);
+                }
+                // Creation  button
+                final Button button = new Button(context);
+                button.setText("Map");
+                button.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getApplicationContext(),"Asignara Coordenadas id:"+id,Toast.LENGTH_LONG).show();
+                    }
+                });
+                row.addView(button);
+                tablaCriaderos.addView(row);
+            }
+        }catch (SQLiteException e){
+            e.printStackTrace();
         }
+        /*finally{
+            daoSession.getDatabase().endTransaction();
+            // End the transaction.
+            //daoSession.getDatabase().close();
+            // Close database
+        }*/
     }
 
-    private void loadSpinerMun(){
-        CtlMunicipio municipio=null;
-        daoMunicipio=daoSession.getCtlMunicipioDao();
-        municipios=new ArrayList<CtlMunicipio>();
-
-        //lista=daoMunicipio.loadAll();
-        municipios=daoMunicipio.queryBuilder().where(CtlMunicipioDao.Properties.IdDepartamento.eq(3))
-                .orderAsc(CtlMunicipioDao.Properties.Nombre).list();
-        obetenerLista();
-    }
-
-    private void obetenerLista() {
-        listaMunicipio=new ArrayList<String>();
-        for (int i=0;i<municipios.size();i++){
-            listaMunicipio.add(municipios.get(i).getNombre());
+    private void encabezadoTabla(Context context){
+        TableRow rowHeader = new TableRow(context);
+        rowHeader.setBackgroundColor(Color.parseColor("#c0c0c0"));
+        rowHeader.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,
+                TableLayout.LayoutParams.WRAP_CONTENT));
+        String[] headerText={"NOMBRE","MUNICIPIO","CANTON","CASERIO","OPCIONES"};
+        for(String c:headerText) {
+            TextView tv = new TextView(context);
+            tv.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                    TableRow.LayoutParams.WRAP_CONTENT));
+            tv.setGravity(Gravity.CENTER);
+            tv.setTextSize(18);
+            tv.setPadding(85, 5, 85, 5);
+            tv.setText(c);
+            rowHeader.addView(tv);
         }
+        tablaCriaderos.addView(rowHeader);
     }
 }
--- app/src/main/java/com/minsal/dtic/sinavec/utilidades/Utilidades.java
+++ app/src/main/java/com/minsal/dtic/sinavec/utilidades/Utilidades.java
@@ -1,5 +1,16 @@
 package com.minsal.dtic.sinavec.utilidades;
 
+import com.minsal.dtic.sinavec.EntityDAO.CtlCanton;
+import com.minsal.dtic.sinavec.EntityDAO.CtlCantonDao;
+import com.minsal.dtic.sinavec.EntityDAO.CtlCaserio;
+import com.minsal.dtic.sinavec.EntityDAO.CtlCaserioDao;
+import com.minsal.dtic.sinavec.EntityDAO.CtlMunicipio;
+import com.minsal.dtic.sinavec.EntityDAO.CtlMunicipioDao;
+import com.minsal.dtic.sinavec.EntityDAO.DaoSession;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by administrador on 07-18-18.
  */
@@ -13,4 +24,75 @@ public class Utilidades {
     public static final int GRID=2;
 
     public static int visualizacion=LIST;
+
+
+    DaoSession daoSession;
+    CtlMunicipioDao daoMunicipio;
+    CtlCantonDao daoCanton;
+    CtlCaserioDao daoCaserio;
+
+    ArrayList<String> listaMunicipio;
+    ArrayList<String> listaCanton;
+    List<CtlCanton> cantones;
+    ArrayList<String> listaCaserios;
+    List<CtlCaserio> caserios;
+
+    public Utilidades(DaoSession daoSession) {
+        this.daoSession = daoSession;
+    }
+
+    public List<CtlMunicipio> loadspinnerMunicipio(int idDepto){
+        CtlMunicipio municipio=null;
+        daoMunicipio=daoSession.getCtlMunicipioDao();
+        List<CtlMunicipio> municipios=new ArrayList<CtlMunicipio>();
+        municipios=daoMunicipio.queryBuilder().where(CtlMunicipioDao.Properties.IdDepartamento.eq(3))
+                .orderAsc(CtlMunicipioDao.Properties.Nombre).list();
+        return municipios;
+    }
+    public ArrayList<String> obtenerListaMunicipio(List<CtlMunicipio> municipios){
+        listaMunicipio=new ArrayList<String>();
+        listaMunicipio.add("Seleccione");
+        for (int i=0;i<municipios.size();i++){
+            listaMunicipio.add(municipios.get(i).getNombre());
+        }
+        return listaMunicipio;
+    }
+
+
+
+
+    public List<CtlCanton> loadSpinerCanton(Long idCtn) {
+        daoCanton=daoSession.getCtlCantonDao();
+        cantones=new ArrayList<CtlCanton>();
+        cantones=daoCanton.queryBuilder().where(CtlCantonDao.Properties.IdMunicipio.eq(idCtn))
+                .orderAsc(CtlCantonDao.Properties.Nombre).list();
+        return cantones;
+    }
+
+    public ArrayList<String> obetenerListaCantones(List<CtlCanton> cantones) {
+        listaCanton=new ArrayList<String>();
+        listaCanton.add("Seleccione");
+        for (int i=0;i<cantones.size();i++){
+            listaCanton.add(cantones.get(i).getNombre());
+        }
+        return listaCanton;
+    }
+
+
+    public List<CtlCaserio> loadSpinerCaserio(Long idCtn) {
+        daoCaserio=daoSession.getCtlCaserioDao();
+        caserios=new ArrayList<CtlCaserio>();
+        caserios=daoCaserio.queryBuilder().where(CtlCaserioDao.Properties.IdCanton.eq(idCtn))
+                .orderAsc(CtlCaserioDao.Properties.Nombre).list();
+        return caserios;
+    }
+
+    public ArrayList<String> obetenerListaCaserios(List<CtlCaserio> caserios) {
+        listaCaserios=new ArrayList<String>();
+        listaCaserios.add("Seleccione");
+        for (int i=0;i<caserios.size();i++){
+            listaCaserios.add(caserios.get(i).getNombre());
+        }
+        return listaCaserios;
+    }
 }
